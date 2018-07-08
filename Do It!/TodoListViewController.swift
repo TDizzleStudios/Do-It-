a9fa6db449226f593d5dfc5f2430f51ab236ff95
T Dizzle <TheRealTDizzle@tylers-macbook-air.local>
2018-07-08 17:26:56 -0500

    allow for items to be checked, deleted didRecieveMemoryWarning

---
+++ Do It!/TodoListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Do It!
+//
+//  Created by T Dizzle on 7/7/18.
+//  Copyright © 2018 T Dizzle Studios LLC. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Do this", "Do that", "Do some of these"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+
+   //MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
